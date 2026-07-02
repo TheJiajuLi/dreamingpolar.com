ed04b658744b2be9756f8035a35b12c7f124b808
Jiaju <mrlijiaju@gmail.com>
2026-07-02 23:44:17 +0100

    Worker加try/catch防止1101错误。

--- _worker.js
+++ _worker.js
@@ -1,18 +1,22 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    if (url.pathname === '/community') {
-      return env.ASSETS.fetch(new Request(new URL('/community.html', url), request));
+    try {
+      if (url.pathname === '/community') {
+        return env.ASSETS.fetch(new Request(new URL('/community.html', url), request));
+      }
+      if (url.pathname.startsWith('/community/user/')) {
+        return env.ASSETS.fetch(new Request(new URL('/profile.html', url), request));
+      }
+      if (url.pathname.startsWith('/community/')) {
+        return env.ASSETS.fetch(new Request(new URL('/tutorial.html', url), request));
+      }
+      if (url.pathname === '/write') {
+        return env.ASSETS.fetch(new Request(new URL('/write.html', url), request));
+      }
+      return env.ASSETS.fetch(request);
+    } catch (e) {
+      return new Response('Not found', { status: 404 });
     }
-    if (url.pathname.startsWith('/community/user/')) {
-      return env.ASSETS.fetch(new Request(new URL('/profile.html', url), request));
-    }
-    if (url.pathname.startsWith('/community/')) {
-      return env.ASSETS.fetch(new Request(new URL('/tutorial.html', url), request));
-    }
-    if (url.pathname === '/write') {
-      return env.ASSETS.fetch(new Request(new URL('/write.html', url), request));
-    }
-    return env.ASSETS.fetch(request);
   },
 };
